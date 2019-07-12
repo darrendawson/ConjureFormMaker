0b552e932c43d4ab83e5648ebab2e008518698da
Darren Dawson <dwdawson@ucsc.edu>
2019-07-12 12:15:06 -0700

    Add USTRA for dealing with App.state memory

--- src/App.js
+++ src/App.js
@@ -6,16 +6,77 @@ import './App.css';
 import Navbar from './Components/Navbar/Navbar.js';
 import FormOutputSidebar from './Components/FormOutputSidebar/FormOutputSidebar.js';
 
+// Ustra - for app.state management
+import Ustra from './Ustra';
+
 // Colors
 import ColorPicker from './ColorPicker.js';
 let COLORS = new ColorPicker('green-blue', 'orange');
+
+
+
+// =============================================================================
+// Fake Data
+// =============================================================================
+/*
+  Data takes the form of: {
+    pagesOrder: [pageKey],                      <--- order of form that that page keys appear in
+    pages: {pageKey: {pageDetails...}}          <--- details about pages
+  }
+*/
+
+
+//
+let fakeFormData = {
+  pagesOrder: ["aboutForm", "submit"],
+  pages: {
+    "aboutForm": {},
+    "submit": {}
+  }
+}
+
+
+// =============================================================================
+// Ustra
+// =============================================================================
+
+// pathTags
+let PT_formData = "formData";
+
+// what App.state will look like
+let dataSkeleton = {
+  [PT_formData]: fakeFormData
+}
+
+// ininitialize USTRA
+var ustra = new Ustra(dataSkeleton);
+
+
+// =============================================================================
+// <App/>
+// =============================================================================
 
 class App extends Component {
 
   constructor() {
     super();
+    this.state = {
+      truth: ustra.get_truth()
+    };
   }
 
+  // Update USTRA --------------------------------------------------------------
+  /*
+    Functions for updating information
+      -> pass updates to ustra, use results to update app.state
+  */
+
+  update = (value, path_tag) => {
+    let new_state = ustra.update(value, path_tag);
+    this.setState({
+      truth: new_state
+    });
+  }
 
   // render --------------------------------------------------------------------
 
@@ -29,7 +90,7 @@ class App extends Component {
         <div id='body_container'>
 
           <div id='left_body_container'>
-
+            <p>{JSON.stringify(this.state.truth[PT_formData])}</p>
           </div>
 
           <div id='right_body_container'>
