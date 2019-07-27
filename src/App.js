2dde35b844ec899a548f357c8fcf35f548cda482
Darren Dawson <dwdawson@ucsc.edu>
2019-07-27 00:16:07 -0700

    Remove deprecated Components <FormItemSidebar/>, <FormMakerPage/>, and <FormOutputSidebar/>. These were remenants of the phased out approach to representing forms and have since been replaced by the current ConjureForm/ConjureFormItem datastructure approach

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import './App.css';
 
 // Components
 import Navbar from './Components/Navbar/Navbar.js';
-import FormOutputSidebar from './Components/FormOutputSidebar/FormOutputSidebar.js';
-import FormMakerPage from './Components/FormMakerPage/FormMakerPage.js';
-import FormItemSidebar from './Components/FormItemSidebar/FormItemSidebar.js';
 import FormSidebar from './Components/FormSidebar/FormSidebar.js';
 
 // Ustra - for app.state management
@@ -14,43 +11,6 @@ import Ustra from './Ustra';
 
 import ConjureForm from './ConjureForm/ConjureForm.js';
 
-
-
-// =============================================================================
-// Fake Data
-// =============================================================================
-/*
-  Data takes the form of: {
-    pagesOrder: [pageKey],                      <--- order of form that that page keys appear in
-    pages: {pageKey: {pageDetails...}}          <--- details about pages
-  }
-*/
-
-
-//
-let fakeFormData = {
-  pageOrder: ["aboutForm"],
-  pages: {
-    "aboutForm": {
-      "sectionOrder": ["aboutSectionCard", "secondSection"],
-      "sections": {
-        "aboutSectionCard": {
-          "itemOrder": ["sectionTitle"],
-          "items": {
-            "sectionTitle": {"itemType": "text", "sectionTitle": "Test", "title": "regular title", "description": "description..."}
-          }
-        },
-
-        "secondSection": {
-          "itemOrder": ["item1"],
-          "items": {
-            "item1": {"itemType": "text", "sectionTitle": "Test", "title": "", "description": ""}
-          }
-        }
-      }
-    }
-  }
-}
 
 
 // =============================================================================
@@ -76,9 +36,6 @@ const defaultColors = {
 
 // what App.state will look like
 let dataSkeleton = {
-  [PT_formData]: fakeFormData,
-  [PT_selectedFormSection]: false,
-
   [PT_conjureForm]: {},
   [PT_selectedFormID]: false,
   [PT_formColors]: defaultColors
@@ -153,94 +110,6 @@ class App extends Component {
     conjureForm.updateAllColors(this.state.truth[PT_formColors]);
     this.update(conjureForm, PT_conjureForm);
   }
-
-  // Form Data -----------------------------------------------------------------
-  /*
-    Functions for dealing with Form data
-  */
-
-  // formData is stored as a large JS object (a dict)
-  // therefore, any item in the form can be reached using an array of keys
-  // -> following this path of keys will bring you to the item
-  // this function returns an item from a form when given a location
-  getFormItemByLocation = (form, location) => {
-    let temp = form;
-
-    for (let i = 0; i < location.length; i++) {
-      let nextLocation = location[i];
-
-      // right now, items are ALWAYS nested as: page.section.item
-      // this is sloppy and will break when we add optional subsections (page.section.[subsection].item)
-      // FIX THIS later
-      if (i === 0) {
-        temp = temp["pages"][nextLocation];
-      } else if (i === 1) {
-        temp = temp["sections"][nextLocation];
-      } else if (i === 2) {
-        temp = temp["items"][nextLocation];
-      }
-    }
-
-    return temp;
-  }
-
-  // this function lets you update an individual form item in the overall app.state.truth.formData
-  updateFormItem = (newItem, location) => {
-
-    // location doesn't include the page / section / item keys used by App.state.truth.formData
-    // ex:
-    // - a form item is at: (app.state...)pages.PAGE_NAME.sections.SECTION_NAME.items.ITEM_NAME
-    // - location will appear as [PAGE_NAME, SECTION_NAME, ITEM_NAME]
-    // We use fullLocation to include these page/section/item keys that are missing
-    let fullLocation = [];
-
-    // function we will use to grab a subobject at a certain location in a larger object
-    // ex: in an object {a: {b: {c: ""}}}, we can get
-    // - (all)    - getObjectAtLocation(object, [])
-    // - (a)      - getObjectAtLocation(object, ["a"])
-    // - (a.b)    - getObjectAtLocation(object, ["a", "b"])
-    let getObjectAtLocation = function(obj, location) {
-      let temp = obj;
-      for (let i = 0; i < location.length; i++) {
-        let nextLocation = location[i];
-        temp = temp[nextLocation];
-      }
-      return temp;
-    }
-
-    // 1) build up fullLocation path
-    for (let i = 0; i < location.length; i++) {
-
-      // right now, items are ALWAYS nested as: page.section.item
-      // this is sloppy and will break when we add optional subsections (page.section.[subsection].item)
-      // FIX THIS later
-      if (i === 0) {
-        fullLocation.push("pages");
-      } else if (i === 1) {
-        fullLocation.push("sections");
-      } else if (i === 2) {
-        fullLocation.push("items");
-      }
-
-      fullLocation.push(location[i]);
-    }
-
-
-    // 2) looping from most specific location (a.b.c.d...x) backwards to most general location (a),
-    //    assign values so the
-    let updatedValue = newItem;
-    for (let i = fullLocation.length - 1; i >= 0; i--) {
-      let currentLocation = fullLocation.slice(0, i);                                         // get the location up to this point in the total object
-      let largerObject = getObjectAtLocation(this.state.truth[PT_formData], currentLocation); // get the object
-      let currentParameter = fullLocation[i];                                                 // largerObject[currentParameter] is the value we are trying to modify
-      largerObject[currentParameter] = updatedValue;
-      updatedValue = largerObject;
-    }
-
-    // updatedValue now stores the fully updated form
-    this.update(updatedValue, PT_formData);
-  }
-
 
 
   // ConjureForm ---------------------------------------------------------------
@@ -367,27 +236,6 @@ class App extends Component {
         onClick_deleteFormSection={this.onClick_deleteFormSection}
       />
     );
-
-    //
-    if (truth[PT_selectedFormSection] !== false) {
-
-      let formItem = this.getFormItemByLocation(truth[PT_formData], truth[PT_selectedFormSection]);
-
-      return (
-        <FormItemSidebar
-          formItem={formItem}
-          selectedItemTag={PT_selectedFormSection}
-          updateFormItem={this.updateFormItem}
-          updateFormItemLocation={truth[PT_selectedFormSection]}
-          update={this.update}
-        />
-      );
-    } else {
-      return (
-        <FormOutputSidebar
-        />
-      );
-    }
   }
 
 
@@ -402,17 +250,6 @@ class App extends Component {
         <div id='body_container'>
 
           <div id='left_body_container'>
-            {/*
-            <FormMakerPage
-              formData={truth[PT_formData]}
-              formDataTag={PT_formData}
-              createNewFormItem={this.createNewFormItem}
-              selectedSection={truth[PT_selectedFormSection]}
-              selectedSectionTag={PT_selectedFormSection}
-              colors={COLORS}
-              update={this.update}
-            />
-            */}
             <div style={{'display': 'flex', 'flex-direction': 'column', 'width': '100%'}}>
               {conjureForm.render()}
             </div>
